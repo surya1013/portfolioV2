0b5be1f434d4ee7b86eb669c8861b6118c7f7583
surya1013 <suryaoffice1013@gmail.com>
2025-04-01 04:34:31 -0400

    Color Schemes implemented
    Color schemes for the website is implemented.

---
+++ src/constants/index.js
@@ -0,0 +1,7 @@
+export const colors = {
+  background: "var(--background)",
+  primary: "var(--primary)",
+  textPrimary: "var(--text-primary)",
+  textSecondary: "var(--text-secondary)",
+  textTertiary: "var(--text-tertiary)",
+};
